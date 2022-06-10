71e668f0aefc5b3ea6be416bfb1cf3fec793ee6c
Jemtaly <83796250+Jemtaly@users.noreply.github.com>
2022-06-10 23:08:49 +0800

    Update bezier.py

--- Python/TkBezier/bezier.py
+++ Python/TkBezier/bezier.py
@@ -7,24 +7,28 @@ def bezier(n, points):
     return [bpoint(i / (n - 1), points) for i in range(n)]
 def main():
     import tkinter
-    points = []
     tk = tkinter.Tk()
     canvas = tkinter.Canvas(tk)
     canvas.pack(fill=tkinter.BOTH, expand=True)
-    def leftclick(event):
+    def initialize(event=None):
+        global points
+        points = []
+        canvas.delete(tkinter.ALL)
+        canvas.bind('<Button-1>', draw_point)
+        canvas.bind('<Double-1>', draw_curve)
+    def draw_point(event):
+        canvas.create_text(event.x, event.y, text=str((event.x, event.y)), fill='blue')
         if points:
             canvas.create_line(*points[-1], event.x, event.y)
-        canvas.create_text(event.x, event.y, text=str((event.x, event.y)), fill='blue')
         points.append((event.x, event.y))
-    def rightclick(event):
-        canvas.unbind('<ButtonPress-1>')
-        canvas.unbind('<ButtonPress-3>')
-        leftclick(event)
+    def draw_curve(event):
+        canvas.unbind('<Button-1>')
+        canvas.unbind('<Double-1>')
         curve = bezier(256, points)
         for p, q in zip(curve[:-1], curve[1:]):
             canvas.create_line(*p, *q, fill='red')
-    canvas.bind('<ButtonPress-1>', leftclick)
-    canvas.bind('<ButtonPress-3>', rightclick)
+    canvas.bind('<Button-3>', initialize)
+    initialize()
     tk.mainloop()
 if __name__ == '__main__':
     main()
