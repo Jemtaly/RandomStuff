cb00db6206b4f776f517ec341935faec5f49b3fd
Jemtaly <83796250+Jemtaly@users.noreply.github.com>
2022-09-06 19:27:21 +0800

    Add files via upload

--- Bash/Brainfuck/bf.sh
+++ Bash/Brainfuck/bf.sh
@@ -4,19 +4,19 @@ func=$(
     do
         case $ch in
         '+')
-            echo -n '((arr[$ptr]++)); '
+            echo -n '((arr[ptr] = arr[ptr] == 255 ? 0 : arr[ptr] + 1)); '
             ;;
         '-')
-            echo -n '((arr[$ptr]--)); '
+            echo -n '((arr[ptr] = arr[ptr] == 0 ? 255 : arr[ptr] - 1)); '
             ;;
         '<')
-            echo -n '((ptr--)); '
+            echo -n '((ptr = ptr == 0 ? 65535 : ptr - 1)); '
             ;;
         '>')
-            echo -n '((ptr++)); '
+            echo -n '((ptr = ptr == 65535 ? 0 : ptr + 1)); '
             ;;
         '[')
-            echo -n 'until [ -z ${arr[$ptr]} ] || [ `expr ${arr[$ptr]} % 256` -eq 0 ]; do :; '
+            echo -n 'until [ -z ${arr[$ptr]} ] || [ ${arr[$ptr]} -eq 0 ]; do :; '
             ;;
         ']')
             echo -n 'done; '
