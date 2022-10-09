f152cebfbbc8a2fbcbaa632d6c493a0d7eb59212
Jemtaly <83796250+Jemtaly@users.noreply.github.com>
2022-10-09 15:42:19 +0800

    Add files via upload

--- C/Insomnia/insomnia.c
+++ C/Insomnia/insomnia.c
@@ -1,13 +1,13 @@
 #include <Windows.h>
 #include <stdio.h>
-#define REC_SYSTEM 1
-#define REC_DISPLAY 2
-#define REC_TIMEOUT 4
+#define REC_TIMEOUT 1
+#define REC_COMMAND 2
+#define REC_DISPLAY 8
 #define REC_ERROR 128
-BOOL timeout(ULONGLONG ullBegin) {
-	fprintf(stderr, "Waiting for %llu milliseconds, press Esc to continue, or press Enter to check the remaining time ...", ullBegin);
+BOOL timeout(ULONGLONG ullPeriod) {
+	fprintf(stderr, "Waiting for %llu milliseconds, press Esc to continue, or press Enter to check the remaining time ...", ullPeriod);
 	HANDLE hStdin = GetStdHandle(STD_INPUT_HANDLE);
-	for (ULONGLONG ullEnd = GetTickCount64() + ullBegin;;) {
+	for (ULONGLONG ullBegin = GetTickCount64(), ullEnd = ullBegin + ullPeriod;;) {
 		if (WaitForSingleObject(hStdin, ullEnd - GetTickCount64()) != WAIT_OBJECT_0) {
 			fprintf(stderr, "\n");
 			return TRUE;
@@ -44,46 +44,53 @@ BOOL pause() {
 	}
 }
 int main(int argc, char *argv[]) {
-	int rec = 0;
+	int rec = 0, ret = 0;
 	unsigned long long t;
+	char *cmd = NULL;
 	for (int i = 1; i < argc; i++) {
 		if (argv[i][0] == '-') {
-			if (argv[i][1] == 't' && argv[i][2] == '\0') {
-				if ((rec & REC_TIMEOUT) == 0 && i + 1 < argc) {
-					t = atoll(argv[++i]);
-					rec |= REC_TIMEOUT;
-				} else {
-					rec |= REC_ERROR;
-				}
-			} else if (argv[i][1] == 'd' && argv[i][2] == '\0') {
-				if ((rec & (REC_DISPLAY | REC_SYSTEM)) == 0) {
+			if (argv[i][1] == 'd' && argv[i][2] == '\0') {
+				if ((rec & REC_DISPLAY) == 0) {
 					rec |= REC_DISPLAY;
 				} else {
 					rec |= REC_ERROR;
 				}
-			} else if (argv[i][1] == 's' && argv[i][2] == '\0') {
-				if ((rec & (REC_DISPLAY | REC_SYSTEM)) == 0) {
-					rec |= REC_SYSTEM;
+			} else if (argv[i][1] == 't' && argv[i][2] == '\0') {
+				if ((rec & (REC_COMMAND | REC_TIMEOUT)) == 0 && i + 1 < argc) {
+					t = atoll(argv[++i]);
+					rec |= REC_TIMEOUT;
 				} else {
 					rec |= REC_ERROR;
 				}
 			} else {
 				rec |= REC_ERROR;
 			}
+		} else if ((rec & (REC_COMMAND | REC_TIMEOUT)) == 0) {
+			cmd = malloc(strlen(argv[i]) + 1);
+			strcpy(cmd, argv[i]);
+			while (++i < argc) {
+				cmd = realloc(cmd, strlen(cmd) + strlen(argv[i]) + 2);
+				strcat(cmd, " ");
+				strcat(cmd, argv[i]);
+			}
+			rec |= REC_COMMAND;
 		} else {
 			rec |= REC_ERROR;
 		}
 	}
-	if ((rec & REC_ERROR) != 0 || (rec & (REC_DISPLAY | REC_SYSTEM)) == 0) {
-		fprintf(stderr, "usage: %s (-s | -d) [-t MILLISECONDS]\n", argv[0]);
+	if ((rec & REC_ERROR) != 0) {
+		fprintf(stderr, "usage: %s [-d] [-t <milliseconds> | <command>]\n", argv[0]);
 		return 1;
 	}
-	SetThreadExecutionState(ES_CONTINUOUS | (rec & REC_DISPLAY ? ES_DISPLAY_REQUIRED : 0) | (rec & REC_SYSTEM ? ES_SYSTEM_REQUIRED : 0));
-	if ((rec & REC_TIMEOUT) != 0) {
+	SetThreadExecutionState(ES_CONTINUOUS | (rec & REC_DISPLAY ? ES_DISPLAY_REQUIRED : ES_SYSTEM_REQUIRED));
+	if ((rec & REC_COMMAND) != 0) {
+		ret = system(cmd);
+		free(cmd);
+	} else if ((rec & REC_TIMEOUT) != 0) {
 		timeout(t);
 	} else {
 		pause();
 	}
-	SetThreadExecutionState(ES_CONTINUOUS);
-	return 0;
+	// SetThreadExecutionState(ES_CONTINUOUS);
+	return ret;
 }
