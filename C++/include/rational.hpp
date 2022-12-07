41963fd11a6643288de9a341b31512b2b8af66ba
Jemtaly <83796250+Jemtaly@users.noreply.github.com>
2022-12-08 01:00:45 +0800

    Add files via upload

--- C++/include/rational.hpp
+++ C++/include/rational.hpp
@@ -1,7 +1,4 @@
 #pragma once
-#include <algorithm>
-#include <string>
-#include <vector>
 class Rational {
 private:
 	int n;
@@ -19,11 +16,6 @@ private:
 			d /= a;
 		}
 	}
-	static constexpr char str36[36] = {
-		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B',
-		'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
-		'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-	};
 public:
 	Rational(int const &numerator = 0, int const &denominator = 1) {
 		if (denominator < 0) {
@@ -53,9 +45,6 @@ public:
 	operator double() const {
 		return value();
 	}
-	inline std::string decimal(uint8_t const &base = 10) const;
-	static inline Rational from_dec(std::string const &str);
-	static inline Rational from_fra(std::string const &str);
 	friend inline Rational operator+(Rational const &);
 	friend inline Rational operator-(Rational const &);
 	friend inline Rational operator~(Rational const &);
@@ -172,90 +161,4 @@ Rational &operator/=(Rational &l, T const &x) {
 template <typename T>
 Rational &operator%=(Rational &l, T const &x) {
 	return l = l % x;
-}
-std::string Rational::decimal(uint8_t const &base) const {
-	if (d == 0) {
-		return n == 0 ? "NaN" : n > 0 ? "Inf" : "-Inf";
-	}
-	std::string result;
-	int integer, decimal;
-	if (n < 0) {
-		integer = -n / d;
-		decimal = -n % d;
-	} else {
-		integer = n / d;
-		decimal = n % d;
-	}
-	do {
-		result += str36[integer % base];
-		integer /= base;
-	} while (integer);
-	if (n < 0) {
-		result += '-';
-	}
-	reverse(result.begin(), result.end());
-	if (decimal) {
-		result += '.';
-		std::vector<int> l;
-		while (decimal && find(l.begin(), l.end(), decimal) == l.end()) {
-			l.push_back(decimal);
-			decimal *= base;
-			decimal %= d;
-		}
-		for (auto i : l) {
-			if (i == decimal) {
-				result += '(';
-			}
-			result += str36[i * base / d];
-		}
-		if (decimal) {
-			result += ')';
-		}
-	}
-	return result;
-}
-Rational Rational::from_fra(std::string const &str) {
-	bool neg = str[0] == '-';
-	int i = str[0] == '-' || str[0] == '+';
-	int n = 0;
-	int d = 0;
-	for (; str[i] >= '0' && str[i] <= '9'; i++) {
-		n *= 10;
-		n += str[i] - '0';
-	}
-	if (str[i] == '/') {
-		for (i++; str[i] >= '0' && str[i] <= '9'; i++) {
-			d *= 10;
-			d += str[i] - '0';
-		}
-	} else {
-		d = 1;
-	}
-	return Rational(neg ? -n : n, d);
-}
-Rational Rational::from_dec(std::string const &str) {
-	bool neg = str[0] == '-';
-	int i = str[0] == '-' || str[0] == '+';
-	int a = 0, b = 0, integer = 0;
-	int m = 1, n = 1;
-	for (; str[i] >= '0' && str[i] <= '9'; i++) {
-		integer *= 10;
-		integer += str[i] - '0';
-	}
-	if (str[i] == '.') {
-		for (i++; str[i] >= '0' && str[i] <= '9'; i++) {
-			m *= 10;
-			a *= 10;
-			a += str[i] - '0';
-		}
-		if (str[i] == '(') {
-			for (i++; str[i] >= '0' && str[i] <= '9'; i++) {
-				n *= 10;
-				b *= 10;
-				b += str[i] - '0';
-			}
-			n--;
-		}
-	}
-	return Rational(neg ? (-integer * m - a) * n - b : (integer * m + a) * n + b, n * m);
 }
