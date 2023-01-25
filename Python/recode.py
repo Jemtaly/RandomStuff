27e3fbe87c754d97cc882acc4d54651b05b7d842
Jemtaly <83796250+Jemtaly@users.noreply.github.com>
2023-01-25 21:48:56 +0800

    Add files via upload

--- Python/recode.py
+++ Python/recode.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
-import argparse
 def main():
+    import argparse
     parser = argparse.ArgumentParser(description = 'Text file recoder.')
-    parser.add_argument('infile', type = argparse.FileType('rb'), help = 'Input file', nargs = '?', default = '-')
-    parser.add_argument('outfile', type = argparse.FileType('wb'), help = 'Output file', nargs = '?', default = '-')
-    parser.add_argument('-d', '--decoding', help = 'Decoding', default = 'utf-8')
-    parser.add_argument('-e', '--encoding', help = 'Encoding', default = 'utf-8')
+    parser.add_argument('-d', '--decoding', help = 'decoding', default = 'utf-8')
+    parser.add_argument('-e', '--encoding', help = 'encoding', default = 'utf-8')
+    parser.add_argument('-i', dest = 'infile', help = 'input file', type = argparse.FileType('rb'), default = '-')
+    parser.add_argument('-o', dest = 'outfile', help = 'output file', type = argparse.FileType('wb'), default = '-')
     args = parser.parse_args()
     args.outfile.write(args.infile.read().decode(args.decoding).encode(args.encoding))
 if __name__ == '__main__':
--- Python/translate.py
+++ Python/translate.py
@@ -5,12 +5,12 @@ def translate(text, source, target):
     ans = requests.get(url).json()[0] or []
     return ''.join(i[0] for i in ans)
 def main():
-    import argparse, sys
-    parser = argparse.ArgumentParser(description = "A simple translator.")
+    import argparse
+    parser = argparse.ArgumentParser(description = "Command Line Translator.")
     parser.add_argument('-s', dest = 'source', help = 'source language', default = 'auto')
     parser.add_argument('-t', dest = 'target', help = 'target language', default = 'auto')
-    parser.add_argument('-i', dest = 'input', help = 'input file', type = argparse.FileType('r'), default = sys.stdin)
-    parser.add_argument('-o', dest = 'output', help = 'output file', type = argparse.FileType('w'), default = sys.stdout)
+    parser.add_argument('-i', dest = 'input', help = 'input file', type = argparse.FileType('r'), default = '-')
+    parser.add_argument('-o', dest = 'output', help = 'output file', type = argparse.FileType('w'), default = '-')
     args = parser.parse_args()
     for line in args.input:
         args.output.write(translate(line, args.source, args.target) + '\n')
